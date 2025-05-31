7caec692cee12a7ab02abbdc58f4ac751056b2fb
ziritrion <ziritrion@gmail.com>
2025-05-31 18:27:17 +0200

    Added methods for saving and retrieving polls to Redis

---
+++ app/services/utils.py
@@ -0,0 +1,20 @@
+from upstash_redis import Redis
+import os
+from uuid import UUID
+from typing import Optional
+
+from app.models.polls import Poll
+
+redis_client = Redis(
+    url=os.getenv("KV_REST_API_URL"),
+    token=os.getenv("KV_REST_API_TOKEN")
+)
+
+def save_poll(poll:Poll) -> Optional[str]:
+    return redis_client.set(key=f"poll:{poll.id}", value=poll.model_dump_json())
+    
+def get_poll(poll_id: UUID) -> Optional[Poll]:
+    poll_json = redis_client.get(key=f"poll:{poll_id}")
+    if poll_json:
+        return Poll.model_validate_json(poll_json)
+    return None
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from fastapi import FastAPI
+from dotenv import load_dotenv
+load_dotenv(dotenv_path='.env')
+
+from fastapi import FastAPI, HTTPException
 from app.models.polls import PollCreate
+from app.services import utils
+from uuid import UUID
 
 app = FastAPI()
 
@@ -10,8 +15,19 @@ def test():
 @app.post("/polls/create")
 def create_poll(poll: PollCreate):
     new_poll = poll.create_poll()
+    utils.save_poll(poll=new_poll)
     return {
         "detail": "Poll successfully created",
         "poll_id": new_poll.id,
         "poll": new_poll
     }
+
+@app.get("/polls/{poll_id}")
+def get_poll(poll_id: UUID):
+    poll = utils.get_poll(poll_id=poll_id)
+    if not poll:
+        raise HTTPException(
+            status_code=404,
+            detail="Cannot retrieve poll: the poll does not exist"
+        )
+    return poll
